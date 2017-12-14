ea878ea4eab3e0debbdb394bc0ca4dd82d7d007a
eleriaqueen <lizard.valeth@gmail.com>
2017-12-14 01:44:36 +0100

    Comments Update

--- pySeccy.py
+++ pySeccy.py
@@ -1,14 +1,14 @@
 C_DESC = "Player-character name can only be 12 characters long and must contain ASCII characters in Classic mode"
 BB_DESC = "Player-character name can only be 10 characters long but can contain unicode characters in Blue-Burst mode"
 NOTASCII = "[!] Name contains non-ASCII characters"
-BB_LEN_REMINDER = "[!] Player-character name can only be 10 characters long in BB mode, input a shorter one: "
 LEN_REMINDER = "[!] Player-character name can only be 12 characters long in Classic mode, input a shorter one: "
+BB_LEN_REMINDER = "[!] Player-character name can only be 10 characters long in BB mode, input a shorter one: "
 TYPE_IN_NAME = "Type in a character name: "
 MODE_SELECT = "(C)lassic or (B)lue-Burst calculation ? "
 
 PSOBB_CLASSES = ['HUmar', 'HUnewearl', 'HUcast', 'HUcaseal', 'RAmar', 'RAmarl', 'RAcast', 'RAcaseal', 'FOmar', 'FOmarl', 'FOnewm', 'FOnewearl']
 PSOBB_CLASSESVAL = [0, 1, 2, 9, 3, 1, 4, 5, 0, 6, 7, 8]
-PSO_LEGACY_CLASSVAL = 5 # Calculations for PSO before BB assume class is a Racaseal
+PSO_LEGACY_CLASSVAL = 5 # Calculations for PSO games older than BB assume class is a Racaseal
 PSO_SECTIONID = ['Pinkal', 'Redria', 'Oran', 'Yellowboze', 'Whitill', 'Viridia', 'Greenill', 'Skyly', 'Bluefull', 'Purplenum']
 
 def unicode_sum ( input_str ):
@@ -32,9 +32,10 @@ if ((modechar == 'c') or (modechar == "C")): # Classic calculation mode
     print(C_DESC)
     while (1):
         charname = input(str(TYPE_IN_NAME))
-        if (isascii(charname)): # Check if all characters in string are ASCII
+        if (isascii(charname)):
             if (len(charname) <= 12):
-                print(PSO_SECTIONID[(unicode_sum(charname) + PSO_LEGACY_CLASSVAL) % 10]) # Get the right section ID in the table and print it
+                # Get the right section ID in the table and print it
+                print(PSO_SECTIONID[(unicode_sum(charname) + PSO_LEGACY_CLASSVAL) % 10])
                 break
             else:
                 print("\n" + LEN_REMINDER + "\n")
