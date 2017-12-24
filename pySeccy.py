a443591d7dc2561424b160de27db537b28eb6607
eleriaqueen <lizard.valeth@gmail.com>
2017-12-24 13:50:08 +0100

    Code Update

--- pySeccy.py
+++ pySeccy.py
@@ -1,7 +1,7 @@
 import sys
 
-USAGE = "usage: hildebear name\n\
-       hildebear [-h] [-f NAME] [-F NAME] [-l]\n\n\
+USAGE = "usage: pySeccy name\n\
+       pySeccy [-h] [-f NAME] [-F NAME] [-l]\n\n\
 optional arguments:\n\
   -h, --help            show this help message and exit\n\
   -fc, --force-classic NAME  compute Section ID\n\
@@ -66,66 +66,83 @@ def BB_PrntSecIDList(name):
 	print("  FOnewm    = " + BB_SectionID(name, BB_CLASSVAL[10]))
 	print("  FOnewearl = " + BB_SectionID(name, BB_CLASSVAL[11]))
 	return 0
+
+def basicMode(arg1):
+	# PSO 'Classic' only accepts up to 12 characters which must be ASCII
+	if (len(arg1) >= 1) and (len(arg1) <= 12) and (len(arg1) == len(arg1.encode())):
+		print("Classic:")
+		print("  " + SectionID(arg1) + "")
+		
+	elif (len(arg1) != len(arg1.encode())):
+		print("Classic: (!) Invalid character(s) detected\n")
+		
+	else: print("Classic: (!) 12-characters limit exceeded\n")
+	
+	# PSO-BB only accepts 10-character names
+	if (len(arg1) <= 10):
+		# We compute Section ID for every class in the game
+		print("\nBlueBurst:")
+		BB_PrntSecIDList(arg1)
+		
+	else: print("\nBlueBurst: (!) 10-characters limit exceeded\n")
+	
+def classicMode(arg2):
+	if (len(arg2) >= 1) and (len(arg2) <= 12) and (len(arg2) == len(arg2.encode())):
+		print(SectionID(arg2))
+			
+	elif len(arg2) != len(arg2.encode()): 
+		print("Classic: (!) Invalid character(s) detected\n")
+			
+	else: print("Classic: (!) 12-characters limit exceeded\n")
+	
+def bbMode(arg2):
+	if (len(arg2) >= 1) and (len(arg2) <= 10):
+		BB_PrntSecIDList(arg2)
+			
+	else: print("\n'" + arg2 + "' (!) 10-characters limit exceeded\n")
+	
+def loopMode():
+	buf = input("Name :\n  ")
+		
+	if (buf == 'exit') or (buf == 'Exit'):
+		return
+		
+	if (len(buf) >= 1) and (len(buf) <= 12) and (len(buf) == len(buf.encode())):
+		print("\nClassic:")
+		print("  " + SectionID(buf))
+			
+	elif (len(buf) != len(buf.encode())):
+		print("\nClassic: (!) Invalid character(s) detected\n")
+		
+	else: print("\n(!) 12-characters limit exceeded\n")
+		
+	if (len(buf) >= 1) and (len(buf) <= 10):
+		print("\nBlueBurst:")
+		BB_PrntSecIDList(buf)
+		print("")
+		
+	else: print("\nBlueBurst: (!) 10-characters limit exceeded\n")
 	
 argNum = len(sys.argv)
 arg = sys.argv
 	
-if (argNum == 2) and not ((arg[1] != '-l') or (arg[1] != '--loop')):
+if (argNum == 2) and not ((arg[1] == '-l') or (arg[1] == '--loop')):
 	if (arg[1] != '-h') and (arg[1] != '--help'):
-		if (len(arg[1]) >= 1) and (len(arg[1]) <= 12):
-			# PSO 'Classic' only accepts up to 12 characters which must be ASCII
-			if len(arg[1]) == len(arg[1].encode()):
-				print("Classic:")
-				print("  " + SectionID(arg[1]) + "")
-			
-			else: print("Classic: (!) Invalid character(s) detected\n")
-		else: print("Classic: (!) 12-characters limit exceeded\n")
-		# PSO-BB only accepts 10-character names
-		if (len(arg[1]) <= 10):
-			# We compute Section ID for every class in the game
-			print("\nBlueBurst:")
-			BB_PrntSecIDList(nameBuf)
-			
-		else: print("\nBlueBurst: (!) 10-characters limit exceeded\n")
+		basicMode(arg[1])
 		
 	else: print(USAGE)
+	
 elif (argNum == 3):
 	if ((arg[1] == '-fc') or (arg[1] == '--force-classic')):
-		if (len(arg[2]) >= 1) and (len(arg[2]) <= 12):
-			if len(arg[2]) == len(arg[2].encode()):
-				print(SectionID(arg[2]))
-			
-			else: print("Classic: (!) Invalid character(s) detected\n")
-		else: print("Classic: (!) 12-characters limit exceeded\n")
+		classicMode(arg[2])
 		
 	elif ((arg[1] == '-fb') or (arg[1] == '--force-bb')):
-		if (len(arg[2]) >= 1) and (len(arg[2]) <= 10):
-			BB_PrntSecIDList(arg[2])
-			
-		else: print("\n'" + arg[2] + "' (!) 10-characters limit exceeded\n")
+		bbMode(arg[2])
 		
 	else: print(USAGE)
 	
 elif (argNum == 2) and ((arg[1] == '-l') or (arg[1] == '--loop')):
 	while True:
-		nameBuf = input("Name :\n  ")
-		
-		if (nameBuf == 'exit') or (nameBuf == 'Exit'):
-			break
-			
-		if (len(nameBuf) >= 1) and (len(nameBuf) <= 12):
-			if len(nameBuf) == len(nameBuf.encode()):
-				print("\nClassic:")
-				print("  " + SectionID(nameBuf))
-				
-			else: print("\nClassic: (!) Invalid character(s) detected\n")
-		else: print("\n(!) 12-characters limit exceeded\n")
-			
-		if (len(nameBuf) >= 1) and (len(nameBuf) <= 10):
-			print("\nBlueBurst:")
-			BB_PrntSecIDList(nameBuf)
-			print("")
-			
-		else: print("\nBlueBurst: (!) 10-characters limit exceeded\n")	
+		loopMode()
 		
 else: print(USAGE)
